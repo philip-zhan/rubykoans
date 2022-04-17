48348183a7a72d848aef4ac794bee6c963e0afb9
Philip Zhan <h6zhan@gmail.com>
2022-04-17 00:45:11 -0700

    day 6 (last day)

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -16,8 +16,33 @@ class Proxy
   def initialize(target_object)
     @object = target_object
     # ADD MORE CODE HERE
+    @messages = []
   end
 
+  def messages
+    @messages
+  end
+
+  def method_missing(method_name, *args, &block)
+    if @object.respond_to?(method_name)
+      @messages.append(method_name)
+      @object.send(method_name, *args)
+    else
+      raise NoMethodError
+    end
+  end
+
+  def called?(method_name)
+    @messages.include?(method_name)
+  end
+
+  def number_of_times_called(method_name)
+    if called?(method_name)
+      @messages.tally[method_name]
+    else
+      0
+    end
+  end
   # WRITE CODE HERE
 end
 
